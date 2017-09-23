7ba1c579d51d4f118c346a093f216b7938209c40
nareshkondaskar <31282485+nareshkondaskar@users.noreply.github.com>
2017-09-23 17:34:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/artical-one', function (req, res) {
+  res.send('artical one requested and served');
+});
+
+app.get('/artical-two', function (req, res) {
+  res.send('artical Two requested and served');
+});
+
+app.get('/artical-three', function (req, res) {
+  res.send('artical Three requested and served');
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
